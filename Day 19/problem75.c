5f6e02173495e3759da66269ffce1a3786a02ff7
Nitin <nitinchaudhary2533@gmail.com>
2026-07-04 18:10:29 +0530

    Day 19 assignment is uploaded

---
+++ Day 19/problem75.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], t[10][10];
+    int i, j, r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    printf("Enter matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of matrix:\n");
+
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+        {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
